35cf0d733bb07fd9edf0cfa3191df5b0ce37c7cb
skrmnyi <carhatt.xm@gmail.com>
2023-02-17 20:24:38 +0100

    rollback to workable state

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,6 @@
 import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -6,31 +8,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstTest {
-
   WebDriver driver;
 
   @Test
+  @BeforeEach
   public void someChecks() {
     System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
-
     driver = new ChromeDriver();
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     driver.get("https://github.com/login");
-
     WebElement loginField = driver.findElement(By.id("login_field"));
     WebElement passwordField = driver.findElement(By.id("password"));
-
     loginField.sendKeys("skrmnyi");
     passwordField.sendKeys("**********");
-
     WebElement signInButton = driver.findElement(By.xpath("//input[@value='Sign in']"));
     signInButton.click();
-
-    WebElement repo = driver.findElement(By.xpath("/html/body/div[1]/div[5]/div/aside/div/loading-context/div/div[1]/div/ul/li[2]/div/div/a"));
+    WebElement repo = driver.findElement(By.xpath(
+        "/html/body/div[1]/div[5]/div/aside/div/loading-context/div/div[1]/div/ul/li[2]/div/div/a"));
     repo.click();
-
-
     WebElement codeTab = driver.findElement(By.id("code-tab"));
     WebElement branchesTab = driver.findElement(By.partialLinkText("branch"));
     WebElement tagsTab = driver.findElement(By.partialLinkText("tags"));
@@ -42,17 +38,10 @@ public class FirstTest {
     WebElement codeDropDownButton = driver.findElement(By.tagName("summary"));
     WebElement addReadmeButton = driver.findElement(By.partialLinkText("README"));
 
-    driver.quit();
-
-    /// Action tab - Xpath's
-
-    WebElement actionTab = driver.findElement(By.xpath("//div//a[@id='actions-tab']"));
-    WebElement setupWorkflowHyperlink = driver.findElement(By.xpath("//a[contains(text(),'set up a workflow')]"));
-    WebElement searchWorkflowInputField = driver.findElement(By.xpath("//input[@aria-label='Search workflows']"));
-    WebElement configureWorkflowButton = driver.findElement(By.xpath("//div/*[contains(@class,'d-flex')]/p[contains(text(),'Start with a file')]" +
-        "/parent::div/following-sibling::div/a[1]"));
-    WebElement veiwAllButtonForDeploymentSection = driver.findElement(By.xpath("//h2[contains(text(),'Deployment')]/parent::div//div[@class='my-3'][contains(text(),'View all')]"));
-
+    }
+    @AfterEach
+    public void quitBroewser () {
+      driver.quit();
   }
 }
 

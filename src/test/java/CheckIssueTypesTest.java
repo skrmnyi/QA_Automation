b082fe67583d02b32c9b78ada2b136bb4cfe0596
skrmnyi <carhatt.xm@gmail.com>
2023-02-18 09:35:44 +0100

    save progress

---
+++ src/test/java/CheckIssueTypesTest.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import pages.IssuesGlobalPage;
+import pages.LoginPage;
+import pages.MainPage;
+
+public class CheckIssueTypesTest extends ConfigTest {
+  @Test
+  public void checkIssueTypesTest() {
+    LoginPage loginPage = new LoginPage(driver);
+    loginPage.successfulLogin("skrmnyi", "*******");
+    MainPage mainPage = new MainPage(driver);
+    mainPage.navigateToIssuesGlobalPage();
+    IssuesGlobalPage issuesGlobalPage = new IssuesGlobalPage(driver);
+    List<String> expectedIssueTypes = new ArrayList<>();
+    expectedIssueTypes.addAll(Arrays.asList("Created", "Assigned", "Mentioned"));
+    Assertions.assertEquals(expectedIssueTypes, issuesGlobalPage.getIssueTypes());
+  }
+}
